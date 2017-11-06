f364c6e94d66a74432b8bbeee57b4bf7fb68e0e0
Matthew Peebles <peebles.mm@gmail.com>
2017-11-05 16:12:02 -0800

    Removed promise from server, added frontend image manipulation

--- public/app.js
+++ public/app.js
@@ -11,51 +11,100 @@ function fetchTopLeftCoordinate(base, logo){
 function onImageFormSubmit(){
 	$('#imageForm').on('submit', (e) => {
 		e.preventDefault()
-			getImages()
-			displayImage()
+			drawFinishedImage()
 	})
 }
 
-function getImages(){
-	let base = $('#base').val()
-	let logo = $('#logo').val()
+function getTopLeftCoordinate(){
+	let base = $('#baseInput').val()
+	let logo = $('#logoInput').val()
 
-	fetchTopLeftCoordinate(base, logo)
-		.then(res => {
-			return res.json()
-		}).then(data => {
-			console.log(data)
-		})
+	return new Promise((resolve, reject) => {
+		fetchTopLeftCoordinate(base, logo)
+			.then(res => {
+				return res.json()
+			}).then(data => {
+				resolve(data)
+			})
+	})
 };
 
 function displayImage(){
-	let images = [
-	{
-		type: 'base',
-		imageSrc: $('#base').val()
-	},
-	{
-		type: 'logo',
-		imageSrc: $('#logo').val()
-	}
-	]
+	
+	return new Promise((resolve, reject) => {
+		let images = [
+		{
+			type: 'base',
+			imageSrc: $('#baseInput').val()
+		},
+		{
+			type: 'logo',
+			imageSrc: $('#logoInput').val()
+		}
+		]
 
 
 
+	    images.forEach(image => {
+	    	let {type, imageSrc} = image
 
-    images.forEach(image => {
-    	let {type, imageSrc} = image
+			let template = `<div id="${type}ImageDiv" class="image">
+						<image src="${imageSrc}" id="${type}" />
+						</div>`
 
-		let template = `<div id="${type}ImageDiv" class="image">
-					<image src="${imageSrc}" id="${type}" />
-					</div>`
+			$('#images').append(template)
+	    })
 
-		$('#images').append(template)
 
-    })
 
+		resolve()
+	})
 }
 
+function getSizeFromLoadedImage(src){
+	return new Promise((resolve, reject) => {
+		let image = new Image()
+
+		image.onload = function(){
+			let height = image.height,
+				width = image.width
+
+			resolve({height, width})
+		}
+
+		image.src = src
+	})
+}
+
+
+function drawFinishedImage(){
+	return getTopLeftCoordinate()
+		.then(topLeft => {
+			return displayImage()
+				.then(() => {
+					return getSizeFromLoadedImage($('#base').attr('src'))
+						.then(baseDimensions => {
+							return getSizeFromLoadedImage($('#logo').attr('src'))
+							.then(logoDimensions => {
+									combineBaseAndLogo(baseDimensions, logoDimensions, topLeft)
+							})
+						})
+				})
+		})
+}
+
+function combineBaseAndLogo(baseDimensions, logoDimensions, topLeft){
+	let canvas = document.getElementById('c')
+	let ctx = canvas.getContext('2d')
+	
+	ctx.canvas.width = baseDimensions.width
+	ctx.canvas.height = baseDimensions.height
+
+	let backgroundImage = document.getElementById('base')
+	let logo = document.getElementById('logo')
+	ctx.drawImage(backgroundImage, 0, 0)
+	ctx.drawImage(logo, topLeft.x, topLeft.y)	
+}
 
 $(() => {
 	onImageFormSubmit()
