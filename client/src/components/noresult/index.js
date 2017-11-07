6b250df636fe92a1a433a037178d240bc57bcbf4
Matthew Peebles <peebles.mm@gmail.com>
2017-11-07 13:33:08 -0800

    Added icon and no result component

---
+++ client/src/components/noresult/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+export default class NoCake extends Component{
+	render(){
+		return(
+			<div>
+				<h2>Sorry</h2>
+				<p>We weren't able to build a cake for you</p>
+				<p>Make sure your logo is smaller than your base</p>
+			</div>
+		)
+	}
+}
